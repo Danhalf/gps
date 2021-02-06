2c0755d0453c0ef4f635750eb08cae5e90c82d0a
Danhalf <danhel@rocketmail.com>
2021-02-06 10:14:14 +0200

    burger

--- #src/js/app.js
+++ #src/js/app.js
@@ -1,5 +1,31 @@
-@@include('files/regular.js', {})
-@@include('files/script.js', {})
-@@include('files/functions.js', {})
-@@include('files/forms.js', {})
-@@include('files/scroll.js', {})
+@@include('files/regular.js', {});
+@@include('files/script.js', {});
+@@include('files/functions.js', {});
+@@include('files/forms.js', {});
+@@include('files/scroll.js', {});
+@@include('libs/swiper.min.js', {});
+@@include('files/dynamic_adapt.js', {});
+
+const navSlide = () => {
+  const burger = document.querySelector('.header__burger');
+  const navMenu = document.querySelector('.header__navigation');
+  const navlinks = document.querySelectorAll('.header__navigation a');
+  const page = document.querySelector('.page');
+
+  burger.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    page.classList.toggle('hide_page');
+    navlinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinksFade 0.5s ease forwards ${
+          index / 5 + 0.5
+        }s`;
+      }
+    });
+
+    burger.classList.toggle('active');
+  });
+};
+navSlide();
